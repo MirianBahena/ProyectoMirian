bb7964598e0949c5be5e88be7b99f9a3c593afa7
Mirian Bahena <104159031+ArtBaMa22@users.noreply.github.com>
2023-03-03 10:30:25 -0600

    Uso del toString

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Student {
-}
+public class Student {
+    private long controlNumber;
+    private String name;
+    private  String lastName;
+    private String career;
+    private int semester;
+
+    public Student() {
+        this.controlNumber = controlNumber;
+        this.name = name;
+        this.lastName = lastName;
+        this.career = career;
+        this.semester = semester;
+    }
+
+    public long getControlNumber() {
+        return controlNumber;
+    }
+
+    public void setControlNumber(long controlNumber) {
+        this.controlNumber = controlNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+
+
+    public String toString (){
+        System.out.println("Los datos del estudiante: ");
+        System.out.println("Nombre: "+ getName());
+        System.out.println("Apellido: "+ getLastName());
+        System.out.println("El numero de control es:"+getControlNumber());
+        System.out.println("El semestre es: "+ getSemester());
+        System.out.println("La carrera es: "+ getCareer());
+        return "";
+    }
+
+    }
+
+
--- src/testStudend.java
+++ src/testStudend.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class testStudend {
+public class testStudend {
+    public static void main(String[] args) {
+        System.out.println("Probando clase Student");
+        Student mirian18 = new Student();
+
+        mirian18.setName("Mirian");
+        mirian18.setLastName("Bahena");
+        mirian18.setCareer("Ingenieria Informatica");
+        mirian18.setControlNumber(22670052L);
+        mirian18.setSemester(2);
+
+        mirian18.toString();
+        mirian18.setName("Juan Pedro");
+        mirian18.toString();
+    }
+
+
+
 }
